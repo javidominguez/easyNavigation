68062a878fa1fabf5c8677ccde658f1652674bf5
Javi Dominguez <javidominguez_github@outlook.es>
2020-05-12 13:06:51 +0200

    Concept and basic structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # easyNavigation
-An addon for NVDA
+
+Substitute single key commands for arrow keys to scroll through headings, links, etc. so that you can do everything with one hand more comfortably and efficiently. This addon is specially designed for facilitates navigation through the elements of a document for people with mobility difficulties but it can also be useful for everyone.
+
+### USAGE
+
+Activate and deactivate  easyNavigation mode by pressing shift + backspace (this gesture can be customized in NVDA preferences).
+
+With easyNavigation mode activated use the left and right arrows to select the type of item and up and down arrows to move to the previous and next item of the selected type.
+
---
+++ buildVars.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "easyNavigation",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("easyNavigation"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""Substitute single key commands for arrow keys to scroll through headings, links, etc. so that you can do everything with one hand."""),
+	# version
+	"addon_version" : "dev20200512",
+	# Author(s)
+	"addon_author" : u"Javi Dominguez <fjavids@gmail.com>",
+	# URL for the add-on documentation support
+	"addon_url" : "https://github.com/javidominguez/easyNavigation",
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0")
+	"addon_minimumNVDAVersion" : None,
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2020.1.0",
+	# Add-on update channel (default is stable or None)
+	"addon_updateChannel" : None,
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = []
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
